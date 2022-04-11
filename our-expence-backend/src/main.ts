c89eb8630cd5e52da92c291af0d8392f195aebc1
Alexis Matamoros <alexismatamoros1996888@gmail.com>
2022-04-11 14:27:49 -0600

    Added API Swagger Documentation

--- our-expence-backend/src/main.ts
+++ our-expence-backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('OUR EXPENCE API')
+    .setDescription('Esta es la documentación para la API Nuestros gastos.')
+    .setVersion('1.0')
+    .addTag('APIS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
 
   app.enableCors();
 
